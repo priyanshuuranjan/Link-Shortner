ca6de26327875730d744dacc32bbf129a8d3fc2d
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-08-27 21:14:20 +0530

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBVMEIjBqVLghZXjY9_dynWfrbF8Waq7v8",
+    authDomain: "link-shortner-ff232.firebaseapp.com",
+    projectId: "link-shortner-ff232",
+    storageBucket: "link-shortner-ff232.appspot.com",
+    messagingSenderId: "123970242151",
+    appId: "1:123970242151:web:a05eb96e84245fc78a76c2",
+  };
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth();
+
+export { app, auth };
